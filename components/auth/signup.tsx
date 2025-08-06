e2290008a853094ffeaa9f115a37dd08e9c7d162
Hyunseok-Cho <hc313615@student.polsl.pl>
2025-08-06 12:35:25 +0200

    Part 5 - Login + 6-digit confirm

---
+++ components/auth/signup.tsx
@@ -0,0 +1,119 @@
+'use client'
+import { createBrowserSupabaseClient } from "@/utils/supabase/client";
+import { Button, Input } from "@material-tailwind/react";
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+
+export default function SignUp({setView}) {
+    const [email, setEmail] = useState("");
+    const [otp, setOtp] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmationRequired, setConfirmationRequired] = useState(false);
+
+    const signInWithKakao = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "kakao",
+      options: {
+        redirectTo: process.env.NEXT_PUBLIC_VERCEL_URL
+          ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/auth/callback`
+          : "http://localhost:3000/auth/callback",
+      },
+    });
+    console.log(data);
+  }
+
+    const supabase = createBrowserSupabaseClient()
+
+    const signupMutation = useMutation({
+        mutationFn: async () => {
+            const {data, error} = await supabase.auth.signUp({
+                email,
+                password,
+                options: {
+                    emailRedirectTo: "http://localhost:3000/signup/confirm",
+                },
+            });
+            
+
+            if(data)
+                setConfirmationRequired(true);
+
+            if(error)
+                alert(error.message);
+        },
+    })
+
+    const verifyOtpmutation = useMutation({
+        mutationFn: async () => {
+            const {data, error} = await supabase.auth.verifyOtp({
+                email,
+                type: 'signup',
+                token: otp,
+            });
+            
+
+            if(data)
+                console.log(data);
+
+            if(error)
+                alert(error.message);
+        },
+    })
+
+    return <div className = "flex flex-col gap-4">
+        <div className="pt-10 pb-6 px-10 w-full flex flex-col items-center 
+            justify-center max-w-lg border border-gray-400 bg-white gap-2">
+            <img src={'/images/inflearngram.png'} className="w-60 mb-6"  />
+            {confirmationRequired ? (<Input
+                value={otp} onChange={(e) => setOtp(e.target.value)}
+                label="otp" type="text"
+                className="w-full rounded-sm"
+                placeholder="6자리 OTP를 입력해주세요."/>) 
+                : (
+                <>
+                <Input
+                value={email} onChange={(e) => setEmail(e.target.value)}
+                label="email" type="email"
+                className="w-full rounded-sm"/>
+
+                <Input
+                value={password} onChange={(e) => setPassword(e.target.value)}
+                label="password" type="password"
+                className="w-full rounded-sm"/>
+                </>
+                )
+            }
+
+            <Button 
+            onClick={()=> {
+                if (confirmationRequired) {
+                    verifyOtpmutation.mutate();
+                } else {
+                    signupMutation.mutate();
+                }
+            }}
+            loading = {confirmationRequired ? verifyOtpmutation.isPending :
+                signupMutation.isPending
+            }
+            disabled={confirmationRequired ? verifyOtpmutation.isPending :
+                signupMutation.isPending}
+            color="light-blue" className="w-full text-md py-1">
+                {confirmationRequired ? "인증하기" : "가입하기"}
+            </Button>
+
+            <Button
+            onClick={() => signInWithKakao()}
+            className="w-full rounded-sm bg-yellow-500">
+                <img src={'/images/kakao.png'} className="w-full " />
+            </Button>
+        </div>
+
+        <div className="py-4 w-full text-center max-w-lg border border-gray-400 bg-white">
+            이미 계정이 있으신가요?
+            <button className="text-light-blue-600 font-bold"
+            onClick={() => {setView("SIGNIN")}}>
+                로그인하기
+            </button>
+        </div>
+    </div>;
+}
